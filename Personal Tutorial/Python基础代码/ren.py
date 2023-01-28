3396f9f3ae64353c8b77eecd2c098de8a8ac3fdb
JunlingZhuang <108850976+JunlingZhuang@users.noreply.github.com>
2023-01-28 15:05:22 -0500

    update

---
+++ Personal Tutorial/Python基础代码/ren.py
@@ -0,0 +1,12 @@
+
+ren_v = 10
+
+def ren_add(ren_list):
+    ren_sum = 0
+    for i in range(len(ren_list)):
+        ren_sum += ren_list[1]
+    return ren_sum
+
+ren_list =  [1,2,3,4,9]
+
+#print (ren_add(ren_list))
